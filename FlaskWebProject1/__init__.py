1416a3fbcb298fa1acfb450bdc85545c50fc7266
Kien Lam <ki3u3q6@l-sfm1zfg8wm-m.gapinc.gap.com>
2017-11-08 22:53:08 -0800

    add bootstrap

--- FlaskWebProject1/__init__.py
+++ FlaskWebProject1/__init__.py
@@ -4,6 +4,12 @@ The flask application package.
 
 from flask import Flask
 from .views.profile import profile
+from flask_bootstrap import Bootstrap
+
+
+bootstrap = Bootstrap()
 
 app = Flask(__name__)
+
+bootstrap.init_app(app)
 app.register_blueprint(profile)
